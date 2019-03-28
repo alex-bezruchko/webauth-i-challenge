8eefb353043f3ff8d330543c90bc7b911ee7ccfa
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-03-28 09:30:59 -0400

    add express session module, import to server file

--- server.js
+++ server.js
@@ -1,13 +1,36 @@
 const express = require('express');
 const server = express();
 const knex = require('knex');
-var bcrypt = require('bcryptjs');
+const bcrypt = require('bcryptjs');
+const session = require('express-session');
+
 server.use(express.json());
 
 const knexConfig = require('./knexfile');
 
 const db = knex(knexConfig.development);
 
+function restricted(req,res,next) {
+    let { username, password } = req.headers;
+    if (username && password) {
+        db('users').where({username}).first()
+        .then(user => {
+            if (user && bcrypt.compareSync(password, user.password)) {
+                next()
+            }
+            else {
+                res.status(401).json({message: 'Invalid credentials'})
+            }
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        })
+    }
+    else {
+        res.status(400).json({message: 'Missing credentials'})
+    }
+    
+}
 server.get('/', (req, res) => {
     res.status(200).json('Home Page up and running')
 });
@@ -41,5 +64,15 @@ server.post('/api/login', (req, res) => {
       });
   });
 
+server.get('/api/users', restricted, (req, res) => {
+    // const hash = bcrypt.hashSync(password, 10)
+
+    // username.password = hash;
+    db('users')
+    .then(users => {
+        res.status(200).json(users)
+    })
+    .catch(err => res.json(err))
+})
 
 module.exports = server;
